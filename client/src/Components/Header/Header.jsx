c3cd7b79f9d1d889ec21d0f935f8d872cd31c465
rkmdCodes <113243168+rkmdCodes@users.noreply.github.com>
2023-04-09 17:32:27 +0530

    Upgraded the Navbar UI

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,31 +1,63 @@
-import { AppBar, Toolbar, Typography, styled } from "@mui/material";
+import { AppBar, Toolbar,Avatar ,Typography, styled,Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const Component = styled(AppBar)`
-  background: #ffffff;
-  color: #000;
+   background: transparent;
+    color: #f913fa ;
+    backdrop-filter: blur(20px) saturate(60%); 
+  background-image:linear-gradient(to bottom right, rgba(0,0,0,0.2), rgba(0,0,0,0.5));
+
+  height: auto ;  
 `;
 
 const Container = styled(Toolbar)`
-  justify-content: center;
-  & > a {
-    padding: 20px;
-    color:inherit;
-    text-decoration:none;
-  }
+    justify-content: left;
+    padding: 0 16px;
+    & > a {
+        padding: 10px;
+        font-size: 14px;
+    }
 `;
+
+
+
+const StyleButton = styled(Button)`
+    text-transform: none;
+    background: transparent;
+    height: 30px;
+    border-radius: 5px;
+    box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.1);
+    margin: 15px;
+    backdrop-filter: blur(20px) grayscale(30%);
+    background-image: linear-gradient(to bottom right, rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5));
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing: 3px;
+    font-family: 'Roboto', sans-serif;
+    color: #FFFFFF;
+    transition: all 0.7s ease;
+
+    &:hover {
+        background: #f913fa;
+        backdrop-filter: blur(75px);
+        background-image: linear-gradient(to bottom right, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0));
+        box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.2);
+    }
+`;
+
 
 const Header = () => {
   return (
     <Component>
-      <Container>
-        <Link to="/">HOME</Link>
-        <Link to="/about">ABOUT</Link>
-        <Link to="/contact">CONTACT</Link>
-        <Link to="/login">LOGOUT</Link>
-      </Container>
+        <Container>
+                    <StyleButton component={Link} to="/">Home</StyleButton>
+                    <StyleButton component={Link} to="/about">About</StyleButton>
+                    <StyleButton component={Link} to="/contact">Connect</StyleButton>
+                    <StyleButton component={Link} to="/account">Account</StyleButton>
+                    <Avatar style={{ marginLeft: 'auto' }} src="/broken-image.jpg" />
+        </Container>
     </Component>
-  );
+)
 };
 
 export default Header;
