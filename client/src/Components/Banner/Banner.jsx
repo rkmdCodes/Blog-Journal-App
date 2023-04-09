fba78a0fbfc36744fce694cc438e20ed94d471d3
rkmdCodes <113243168+rkmdCodes@users.noreply.github.com>
2023-04-09 17:34:56 +0530

    Updated the Banner Image

--- client/src/Components/Banner/Banner.jsx
+++ client/src/Components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import {Box,Typography,styled} from '@mui/material';
 
 const Image = styled(Box)`
 width: 100%;
-background: url(https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg) center/55% repeat-x #000;
+background: url(https://i.pinimg.com/originals/c0/9b/92/c09b92df282811f76db635f9bb15eac2.jpg) center/100% repeat-x #000;
 height: 50vh;
 display: flex;
 flex-direction: column;
